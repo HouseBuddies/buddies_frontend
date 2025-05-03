49a2aac980324e54c5557f99a3648419af767225
João Lobo <joao@lobo.is>
2025-05-03 03:52:09 +0100

    fix: center age input

--- screens/DemographicsScreen.tsx
+++ screens/DemographicsScreen.tsx
@@ -28,7 +28,7 @@ export default function DemographicsScreen({ navigation, route }) {
         <View className="mb-8">
           <Text className="text-lg font-semibold text-primary mb-2">Age</Text>
           <TextInput
-            className="border border-gray-300 rounded-lg px-4 pb-2 pt-2 text-lg"
+            className="border border-gray-300 rounded-lg pt-2 pb-4 px-4 text-lg"
             keyboardType="numeric"
             value={age.toString()}
             onChangeText={(text) => {
